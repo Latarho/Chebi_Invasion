c6e5e777634f71c755a2378c039521d674bd86db
Latarho <Latarho2@gmail.com>
2021-03-10 20:06:04 +0300

    Создание класса Bullet.

--- chebi_invasion.py
+++ chebi_invasion.py
@@ -1,8 +1,10 @@
 import sys
 import pygame
 
+from pygame.sprite import Group
 from settings import Settings
 from ship import Ship
+
 import game_functions as gf
 
 
@@ -14,9 +16,13 @@ def run_game():
     pygame.display.set_caption("Chebi Invasion")
     # Создание корабля.
     ship = Ship(ai_settings, screen)
+    # Создание группы для хранения пуль.
+    bullets = Group()
     # Запуск основного цикла игры.
     while True:
-        gf.check_events(ship)
+        gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
-        gf.update_screen(ai_settings, screen, ship)
+        gf.update_bullets(bullets)
+        # Удаление пуль, вышедших за край экрана.
+        gf.update_screen(ai_settings, screen, ship, bullets)
 run_game()
--- settings.py
+++ settings.py
@@ -8,4 +8,10 @@ class Settings():
         self.screen_height = 800
         self.bg_color = (255, 255, 255)
         # Настройки корабля.
-        self.ship_speed_factor = 2
+        self.ship_speed_factor = 1
+        # Параметры пули
+        self.bullet_speed_factor = 1
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = 60, 60, 60
+        self.bullets_allowed = 3
